a650907ea0ffd3de878c7d88e43a2dcde911d55d
Gaunacamila <phcamila.gauna@gmail.com>
2023-01-30 14:03:54 -0300

    Primer Commit

---
+++ AsistenteVirtual/README.md
@@ -0,0 +1,13 @@
+# AsistenteVirtual
+Asistente Virtual con Python
+
+Camila
+
+Descargar : Python SpeechRecogniotion (Libreria)
+	    pyttsx3 2.90
+	    PyAudio 0.2.13
+
+ 
+Copy : pip install SpeechRecognition
+	pip install pyttsx3
+	pip install PyAudio
---
+++ AsistenteVirtual/jarvis.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener = sr.Recognizer()
+    ##Reconoce la voz
+engine = pyttsx3.init()
+##inicializar
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[2].id)
+#cambiar la voz
+
+
+engine.say("Buenos días mi amor")
+engine.runAndWait()
+try:
+    with sr.Microphone() as source:
+    #Llama a la función Microphone as source :
+                print("habla Guei")
+                voice = listener.listen(source)
+                rec = listener.recognize_google(voice)
+                print(rec)
+except:
+    pass
+#Ignorar  prueba
